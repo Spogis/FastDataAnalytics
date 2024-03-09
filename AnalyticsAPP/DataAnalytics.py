5238eb4ad59db82cef7fac062d7466e768595d72
Nicolas Spogis <nicolas.spogis@gmail.com>
2024-03-09 15:54:15 -0300

    Initial commit

---
+++ AnalyticsAPP/DataAnalytics.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from ydata_profiling import ProfileReport
+
+def data_analytics(df):
+    html_file = 'assets/relatorio_analise.html'
+    if os.path.exists(html_file):
+        os.remove(html_file)
+
+    # Carregue seus dados do arquivo Excel
+    dados = df
+
+    profile_report = ProfileReport(
+        dados,
+        sort=None,
+        html={
+            "style": {"full_width": True}
+        },
+        progress_bar=True,
+        correlations={
+            "auto": {"calculate": True},
+        },
+        explorative=True,
+        interactions={"continuous": True},
+        title="Profiling Report"
+    )
+
+    # Gere o relatório em HTML
+    profile_report.to_file(html_file)
